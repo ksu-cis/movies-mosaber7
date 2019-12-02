b4b8e61a37ee7eecdd3798ba0f9c56bb62b108a5
Mohamed Fares <mosaber7@users.campus>
2019-12-02 16:47:07 -0600

    fuck

--- Movies/Pages/Index.cshtml.cs
+++ Movies/Pages/Index.cshtml.cs
@@ -20,7 +20,8 @@ namespace Movies.Pages
         public float? maxIMDB { get; set; }
         [BindProperty]
         public string search { get; set; }
-
+        [BindingProperty]
+        public 
 
 
         public void OnGet()
@@ -44,26 +45,62 @@ namespace Movies.Pages
 
            else if ( mpaa.Count != 0)
             {
-                Movies = MovieDatabase.Filter(mpaa);
+          Movies=Movies.where(Movie=> mpaa.contains(Movie.MPAA_Rating));
+                
+                
+                //      Movies = MovieDatabase.Filter(mpaa);
             }
             else if (search != null )
             {
-                Movies = movieDataBase.search(Movies,search);
+                Movies=Movies.where(Movie=> Movie.Title.contains(search, stringComparsion.OrdinalIgnoreCase));
             }
             if (minIMDB != null)
             {
-
-                Movies = MovieDatabase.FilterByMinIMBD(Movies, minIMDB);
+                Movies=Movies.where(Movie=>Movie.IMDB_Ratung!=null && Movie.IMBD_Rating);
+              //  Movies = MovieDatabase.FilterByMinIMBD(Movies, minIMDB);
             }
             if (maxIMDB != null)
             {
-
-                Movies = MovieDatabase.FilterByMinIMBD(Movies, maxIMDB);
+                Movies=Movies.where(Movie=>Movie.IMDB_Ratung!=null && Movie.IMBD_Rating);
+               // Movies = MovieDatabase.FilterByMinIMBD(Movies, maxIMDB);
             }
+            
             else
             {
                 Movies = MovieDatabase.All;
             }
+            switch(sort)
+            {
+                case "title":
+                    Movies=Movies.orderBy(Movie=>Movie.title);
+                    break;
+                case "director":
+                     Movies=Movies
+                        .where(Movie=>Movie.director!= null)
+                        .orderBy(Movie=>{
+                    string []parts=Movie.Director.split(" ");
+                    return parts[parts.lenghts-1];
+                        });
+                    break;
+                case "mppa":
+                     Movies=Movies.orderBy(Movie=>Movie.MPAA_Rating);
+                    break;
+                case "imdb":
+                     Movies=Movies.orderBy(Movie=>Movie.IMDB_Rating);
+                    break;
+                case "rt":
+                     Movies=Movies
+                        .Where(Movie=> Movie.Rotten_Tomatoes_Rating!=null)
+                        .OrderBy(Movie=>Movie)
+
+
+            }
+
+        }
+        public static List<Movie> Sort(List<Movie> movies)
+        {
+
+
 
 
         }
